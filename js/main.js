f70afffcbce9c78c2acedf052b0d7876eff0c588
RahulWala <walarahul08@gmail.com>
2017-09-26 18:57:16 +0530

    Maade some changes

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.carousel').carousel({
+  interval: 2000
+})
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+    document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
